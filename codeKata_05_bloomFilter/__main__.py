93d87c9401c974369edc3d0a532c4a729ad35151
keane <keane.leeds@gmail.com>
2020-10-25 11:49:33 -0500

    Broke __main__.py down into testable functions and commented. Added commandline parsing. Stubbed out characterizer. Made bloom filter characteristic values settable with commandline arguments.

--- codeKata_05_bloomFilter/__main__.py
+++ codeKata_05_bloomFilter/__main__.py
@@ -5,26 +5,164 @@ import numpy as np
 import nltk
 from nltk.corpus import words
 from functools import partial
+import argparse
 
 
-def produce_hash_function(prefix: int, length: int, text: str):
+A = ''
+n: int = 0
+m: int = 0
+k: int = 0
+v: np.ndarray = []
+H = []
+
+
+def import_nltk_corpus_words() -> None:
+    """Imports the words corpus from nltk
+
+    the words corpus from nltk is 234377 unique english words
+    """
+
+    nltk.download('words', quiet=True)
+
+
+def produce_hash_function(prefix: int, length: int, text: str) -> str:
     """Returns a hash string large enough to cover size m
 
     Produces a hexadecimal digest from a sha256 hash of the input string,
     prefixed with the value of argument prefix, and slices the desired
     number of characters from it
     """
-    return hashlib.sha3_256(f'{prefix}text'.encode('utf-8')).hexdigest()[:length]
+
+    return hashlib.sha3_256(f'{prefix}{text}'.encode('utf-8')).hexdigest()[:length]
 
 
-def already_exists_in_bloom_filter(entry):
+def create_hashes():
+    """Creates a list of hash functions that only need a text argument
+
+    the hash functions set the prefix and length arguments of the produce_hash_function function
+    """
+
+    return [partial(produce_hash_function, prefix, (m // 16) + 1) for prefix in range(k)]
+
+
+def fill_bloom_filter() -> None:
+    """Creates the bloom filter
+
+    for each a ∈ A the bits in positions v[h_1(a)], v[h_2(a)],..., v[h_k(a)] are set to 1
+    """
+
+    for a in A:
+        for h in range(k):
+            v[int(H[h](a), 16) % m] = 1  # test this, I think the mod is wrong
+
+
+def default_words_test() -> None:
+    """A small set of words to use as a sanity test for the bloom filter"""
+    print(f'f: {already_exists_in_bloom_filter("f")}')
+    print(f'zed: {already_exists_in_bloom_filter("zed")}')
+    print(f'wonderful: {already_exists_in_bloom_filter("wonderful")}')
+    print(f'random: {already_exists_in_bloom_filter("wonderful")}')
+    print(f'indubitably: {already_exists_in_bloom_filter("indubitably")}')
+
+
+def random_words_test(number_of_tests: int) -> None:
+    """Random, 5-character words to use to check the false drop frequency"""
+    for _ in range(number_of_tests):
+        random_word = ''.join(random.choice(string.ascii_letters) for _ in range(5))
+        print(f'{random_word}: {already_exists_in_bloom_filter(random_word)}')
+
+
+def already_exists_in_bloom_filter(entry: str) -> bool:
     """Checks if entry already exists in the bloom filter"""
+
     hashes = np.array([v[int(H[h](entry), 16) % m] for h in range(k)])
     return np.all(hashes == 1)
 
 
+def characterize_bloom_filter() -> None:
+    raise NotImplemented('Not implemented yet')
+
+
+def get_args() -> argparse.Namespace:
+    """Parses commandline arguments"""
+    parser = argparse.ArgumentParser(
+        prog='bloomFilter',
+        description='Create a bloom filter, or characterize the bloom filter')
+    parser.add_argument(
+        '-A',
+        default='',
+        type=str,
+        dest='A',
+        help='a file, or list of words to add to the bloom filter')
+    parser.add_argument(
+        '-m',
+        default=0,
+        type=int,
+        dest='m',
+        help='the size of the hash output')
+    parser.add_argument(
+        '-k',
+        default=4,
+        type=int,
+        dest='k',
+        help='the number of independent hash functions')
+    parser.add_argument(
+        '-t', '--tests',
+        default=10,
+        type=int,
+        dest='number_of_tests',
+        help='the number of 5 character tests to run')
+    parser.add_argument(
+        '--runTests',
+        default=True,
+        type=bool,
+        dest='run_tests',
+        help='run the 5 characters test')
+    parser.add_argument(
+        '--defaultTests',
+        default=False,
+        type=bool,
+        dest='default_tests',
+        help='run the default word tests or not')
+    parser.add_argument(
+        '--characterize',
+        action='store_true',
+        help='Characterize the bloom filter with respect to false drops')
+    return parser.parse_args()
+
+
+def set_bloom_filter_values() -> None:
+    """Sets the values that characterize a given bloom filter"""
+    global A, n, m, v, k, H
+
+    if args.A == '':
+        import_nltk_corpus_words()
+        # remove duplicates based on capitalization and convert back to a list
+        A = list(set(i.lower() for i in words.words()))
+        # sort the list alphabetically
+        A.sort()
+    elif args.A.endswith('.txt'):
+        file = open(args.A, 'r')
+        A = file.read()
+        file.close()
+    else:
+        raise ValueError(f'Unsupported document type: {args.A}')
+
+    n = len(A)
+
+    if args.m == 0:
+        m = n*4
+    else:
+        m = args.m
+
+    v = np.zeros(m)
+    k = args.k
+    H = create_hashes()
+
+
 if __name__ == '__main__':
-    """
+    """Implementation of a bloom filter
+    
     For set A = {a_1, a_2, ..., a_n}
     create a vector v of size m, initialized to 0
     create k independent hash functions h_1,h_2,...,h_k, with range 1..m
@@ -32,26 +170,19 @@ if __name__ == '__main__':
     
     to check if an input is already in the bloom filter, check the array locations for zeros.
     """
-    nltk.download('words', quiet=True)
-    A = words.words()
-    n = len(A)
-    m = n*4
-    v = np.zeros(m)
-    k = 4
-    H = [partial(produce_hash_function, h, (m // 10) + 1) for h in range(k)]
 
-    for a in A:
-        for h in range(k):
-            v[int(H[h](a), 16) % m] = 1
+    args = get_args()
+    set_bloom_filter_values()
 
-    print(f'f: {already_exists_in_bloom_filter("f")}')
-    print(f'zed: {already_exists_in_bloom_filter("zed")}')
-    print(f'wonderful: {already_exists_in_bloom_filter("wonderful")}')
-    print(f'random: {already_exists_in_bloom_filter("wonderful")}')
-    print(f'indubitably: {already_exists_in_bloom_filter("indubitably")}')
+    fill_bloom_filter()
 
-    for _ in range(10):
-        random_word = ''.join(random.choice(string.ascii_letters) for _ in range(5))
-        print(f'{random_word}: {already_exists_in_bloom_filter(random_word)}')
+    if args.default_tests:
+        default_words_test()
+
+    if args.run_tests:
+        random_words_test(args.number_of_tests)
+
+    if args.characterize:
+        characterize_bloom_filter()
 
     print('Done')
--- codeKata_05_bloomFilter/tests/test_produce_hash_function.py
+++ codeKata_05_bloomFilter/tests/test_produce_hash_function.py
@@ -3,18 +3,55 @@ import codeKata_05_bloomFilter as bF
 
 
 class TestProduceHashFunction(unittest.TestCase):
-    """
-    hashlib.sha224('bork'.encode('utf-8')).hexdigest() produces the string
-        '33f8294eed104d7aded64d81d298ad9a2fbebf59176172ec4be65a29'
-    """
-    def test_produce_hash_function_gets_correct_index(self):
-        self.assertEqual(bF.produce_hash_function(0, 1, 'bork'), '3')
-        self.assertEqual(bF.produce_hash_function(1, 1, 'bork'), '3')
-        self.assertEqual(bF.produce_hash_function(2, 1, 'bork'), 'f')
-        self.assertEqual(bF.produce_hash_function(3, 1, 'bork'), '8')
-        self.assertEqual(bF.produce_hash_function(4, 1, 'bork'), '2')
-        self.assertEqual(bF.produce_hash_function(5, 1, 'bork'), '9')
-        self.assertEqual(bF.produce_hash_function(6, 1, 'bork'), '4')
 
-        self.assertEqual(bF.produce_hash_function(0, 5, 'bork'), '33f82')
-        self.assertEqual(bF.produce_hash_function(55, 1, 'bork'), '9')
+    def test_inputs_bork_0_1(self):
+        self.assertEqual('a2759bbbc506c841737b4285a457deaa23865660338dd568f9ebadc4f1e1067b',
+                         bF.produce_hash_function(0, 64, 'bork'))
+        self.assertEqual('a', bF.produce_hash_function(0, 1, 'bork'))
+
+    def test_inputs_bork_1_1(self):
+        self.assertEqual('decda4e39f1beb4ab32ee478fe1b429fccb569ac49602737ec7b293db0e39cf0',
+                         bF.produce_hash_function(1, 64, 'bork'))
+        self.assertEqual('d', bF.produce_hash_function(1, 1, 'bork'))
+
+    def test_inputs_bork_2_1(self):
+        self.assertEqual('fb63227dd8b2bd88b17bd7c4bd717a2c4b1937ed8f38a5f989323e975426a33f',
+                         bF.produce_hash_function(2, 64, 'bork'))
+        self.assertEqual('f', bF.produce_hash_function(2, 1, 'bork'))
+
+    def test_inputs_bork_3_1(self):
+        self.assertEqual('eb9470c8a67b62ab9ec2c2c4eda534d5b17f5d4d5e3b152b87c012286768514d',
+                         bF.produce_hash_function(3, 64, 'bork'))
+        self.assertEqual('e', bF.produce_hash_function(3, 1, 'bork'))
+
+    def test_inputs_bork_4_1(self):
+        self.assertEqual('cd3053dc875afd439f8bd21c09c4129261a6e24d8c9d3cc068c53c739e53ac3a',
+                         bF.produce_hash_function(4, 64, 'bork'))
+        self.assertEqual('c', bF.produce_hash_function(4, 1, 'bork'))
+
+    def test_inputs_bork_5_1(self):
+        self.assertEqual('0685f0f6f3920fea47a8bebc58e095677f08f49600c3e8aa131c09dd2a395f56',
+                         bF.produce_hash_function(5, 64, 'bork'))
+        self.assertEqual('0', bF.produce_hash_function(5, 1, 'bork'))
+
+    def test_inputs_bork_6_1(self):
+        self.assertEqual('1f3cd989d0843b96f68dc43102a3142d71d591b5b2bb3c571913862cd1e5848e',
+                         bF.produce_hash_function(6, 64, 'bork'))
+        self.assertEqual('1', bF.produce_hash_function(6, 1, 'bork'))
+
+    def test_inputs_bork_0_5(self):
+        self.assertEqual('a2759', bF.produce_hash_function(0, 5, 'bork'))
+
+    def test_inputs_bork_0_64(self):
+        self.assertEqual(64, len('a2759bbbc506c841737b4285a457deaa23865660338dd568f9ebadc4f1e1067b'))
+        self.assertEqual(64, len(bF.produce_hash_function(0, 64, 'bork')))
+
+    def test_inputs_bork_0_64(self):
+        self.assertEqual('a2759bbbc506c841737b4285a457deaa23865660338dd568f9ebadc4f1e1067b',
+                         bF.produce_hash_function(0, 65, 'bork'))
+        self.assertEqual(64, len(bF.produce_hash_function(0, 65, 'bork')))
+        # this will work fine for bloom filters that need an m value of less than 16^64
+
+
+if __name__ == '__main__':
+    unittest.main()
